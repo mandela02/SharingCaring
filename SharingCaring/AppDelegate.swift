21726658097ddcbbe8d0472323b1a8c06f174d0f
TriBQ <10khongquang@gmail.com>
2020-09-20 02:00:55 +0700

    init project and add pod

---
+++ SharingCaring/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SharingCaring
+//
+//  Created by TriBQ on 9/20/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
